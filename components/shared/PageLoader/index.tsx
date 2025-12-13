39ea0178007ead59adb2f3c4fc40713a54fba549
Yu Tong Liu <yutongliuytl@gmail.com>
2025-12-13 08:31:17 +0800

    Adds better styling

--- components/shared/PageLoader/index.tsx
+++ components/shared/PageLoader/index.tsx
@@ -5,10 +5,7 @@ import styles from "./styles.module.scss";
 const PageLoader = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.logoWrapper}>
-        <Image src="/logo.svg" alt="EZPZ Coffee" width={120} height={97} priority className={styles.logo} />
-        <div className={styles.shimmer} />
-      </div>
+      <Image src="/logo.svg" alt="EZPZ Coffee" width={80} height={65} priority className={styles.logo} />
     </div>
   );
 };
